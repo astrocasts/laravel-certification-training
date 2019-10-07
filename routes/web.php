aa1293b8ac4b5cd5dbaac34c45f6e0e12d867d43
Beau Simensen <beau@dflydev.com>
2019-10-07 18:03:39 -0400

    Add web routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/first', function () {
+    return 'Hello World!';
+});
+
+Route::get('user/{id}', function ($id) {
+    return 'Hello, user ID '. $id . '!';
+})->where('id', '[0-9]+');
+
+Route::get('user/{name}', function ($name) {
+    return 'Hello, user name '. $name . '!';
+})->where('name', '[A-Za-z]+');
+
+Route::get('user/{name?}', function ($name = null) {
+    return 'Hello, user name last '. $name . '!';
+});
+
+Route::get('user/{name?}', function ($name = null) {
+    return 'Hello, user name LAST '. $name . '!';
+});
+
+Route::get('users/{username}', function ($username) {
+    return route('user.show', [
+        'username' => $username,
+        'label' => 'inbox',
+    ]);
+})->name('user.show');
